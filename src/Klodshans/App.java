92dca193da7f56fa8b432ef93ee864ae903d9fc2
rasmusjerlov <rasmus.jerloev@gmail.com>
2023-04-08 13:30:38 +0200

    Latest update

--- src/Klodshans/App.java
+++ src/Klodshans/App.java
@@ -1,11 +1,65 @@
 package Klodshans;
 
+import java.util.*;
+
 public class App {
 
     public static void main(String[] args) {
-        GUI.launch(GUI.class);
-        for (Deltager i : Storage.deltagere) {
-            System.out.println(i.toString());
+        System.out.println("Indtast antal deltagere: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < n; i++) {
+            Deltager d = new Deltager(scanner.nextLine());
+            Storage.deltagere.add(d);
+            d.setId(i + 1);
         }
+
+
+        System.out.println("Klodshans menu");
+        System.out.println("Vælg deltager:");
+        for (Deltager i : Storage.deltagere) {
+            System.out.print("ID " + i.getId() + ": ");
+            System.out.println(i.getName());
+        }
+
+        System.out.println("Indtast deltager id for at vælge");
+        int deltager = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Du valgte " + Storage.deltagere.get(deltager - 1).getName());
+        System.out.println("Hvad vil du tilføje? ");
+        System.out.println("1: Øl");
+        System.out.println("2: Snus");
+        System.out.println("3: Shots");
+        int x = scanner.nextInt();
+        if (x == 1) {
+            System.out.println("Hvor mange øl har " +
+                    Storage.deltagere.get(deltager - 1).getName() +
+                    " klodset?");
+            int y = scanner.nextInt();
+            Storage.deltagere.get(deltager - 1).setAntalØl(y);
+            System.out.println(Storage.deltagere.get(deltager - 1).getAntalØl());
+        } else if (x == 2) {
+            System.out.println("Hvor mange skiver har " +
+                    Storage.deltagere.get(deltager - 1).getName() +
+                    " klodset?");
+            int y = scanner.nextInt();
+            Storage.deltagere.get(deltager - 1).setAntalSnus(y);
+            System.out.println(Storage.deltagere.get(deltager - 1).getAntalSnus());
+        } else if (x == 3) {
+            System.out.println("Hvor mange shots har " +
+                    Storage.deltagere.get(deltager - 1).getName() +
+                    " klodset?");
+            int y = scanner.nextInt();
+            Storage.deltagere.get(deltager - 1).setAntalShots(y);
+            System.out.println(Storage.deltagere.get(deltager - 1).getAntalShots());
+
+        } else {
+
+        }
+
+
+//        GUI.launch(GUI.class);
+
     }
 }
--- src/Klodshans/Storage.java
+++ src/Klodshans/Storage.java
@@ -1,7 +1,9 @@
 package Klodshans;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Storage {
-    public static ArrayList<Deltager> deltagere = new ArrayList<Deltager>();
+    public static ArrayList<Deltager> deltagere = new ArrayList<>();
+
 }
